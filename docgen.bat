4a1310cfa6e3d5d3c1bd2678a2d206899837a806
ChamiLamelas <chami.lamelas@gmail.com>
2022-01-24 00:05:49 -0500

    moved up docs folder

--- docgen.bat
+++ docgen.bat
@@ -1,2 +1,2 @@
 @echo off
-pdoc3 --html --config show_source_code=False -o ./src/cormen_lib/docs ./src/cormen_lib --force
+pdoc3 --html --config show_source_code=False -o ./docs ./src/cormen_lib --force
