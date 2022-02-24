b5aceb41d798fd424fedd43ad33730e8578eebc7
eitanjoseph <eitan17j@gmail.com>
2022-02-24 18:05:53 -0500

    updating Cormen-lib -> Cormen-Lib

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Cormen-Lib began as a project by two Brandeis University undergraduate students 
 written in pseudocode.
 
 ## Provided Data Structures
-The Cormen-lib library offers a set of fundamental data structures and algorithms, with behavior as specified by
+The Cormen-Lib library offers a set of fundamental data structures and algorithms, with behavior as specified by
 Cormen et al.'s *Introduction to Algorithms*. The following structures (separated by module) are supported:
 
 * [arrays](https://cormen-lib-developers.github.io/Cormen-Lib/arrays.html)
@@ -44,7 +44,7 @@ Cormen et al.'s *Introduction to Algorithms*. The following structures (separate
     * Graph
 
 ## Unit Testing
-Along with the Cormen-lib data structures come test utilities for writing test cases. The testing framework allows a
+Along with the Cormen-Lib data structures come test utilities for writing test cases. The testing framework allows a
 course administrator to easily write test cases for either expected function output or general class behavior. Test cases
 can then be combined into a testing suite. The testing suite has the capability to set a test case run-time timeout and 
 to record comma-separated test results for administrative use.
@@ -74,11 +74,11 @@ if __name__ == '__main__':
 
 ## Installation
 
-Cormen-lib is [available on PyPI](https://pypi.org/project/cormen-lib/), and can be installed with pip.
+Cormen-Lib is [available on PyPI](https://pypi.org/project/cormen-lib/), and can be installed with pip.
 
     pip install cormen-lib
 
-Cormen-lib has the following dependencies:
+Cormen-Lib has the following dependencies:
 
     Python >= 3.6
 
@@ -89,12 +89,12 @@ correctness, documentation and feature requests.
 
 ## Academic Usage
 
-If you are planning to use Cormen-lib for a university course and have questions, feel free to reach out by email.
+If you are planning to use Cormen-Lib for a university course and have questions, feel free to reach out by email.
 
 ## Documentation
 
-The full documentation for Cormen-lib is available [here](https://cormen-lib-developers.github.io/Cormen-Lib/).
+The full documentation for Cormen-Lib is available [here](https://cormen-lib-developers.github.io/Cormen-Lib/).
 
 ## Sample Usage
 
-To view sample assignments using Cormen-lib browse the [Cormen-lib sample problems repository](https://github.com/Cormen-Lib-Developers/Cormen-Lib-Sample-Problems) on github.
+To view sample assignments using Cormen-Lib browse the [Cormen-Lib sample problems repository](https://github.com/Cormen-Lib-Developers/Cormen-Lib-Sample-Problems) on github.
--- src/cormen_lib/__init__.py
+++ src/cormen_lib/__init__.py
@@ -1,4 +1,4 @@
-"""The Cormen-lib module is a data structures and algorithms library based on Thomas H. Cormen et al.'s
+"""The Cormen-Lib module is a data structures and algorithms library based on Thomas H. Cormen et al.'s
 *Introduction to Algorithms Third Edition*. This library was made specifically for administering and grading assignments
 related to data structures and algorithms in computer science.
 
@@ -6,15 +6,15 @@ With this module students can receive progress reports on their problem sets in 
 Additionally, student submission assessment is done with unit tests, instead of hand-tracing, ensuring that the grades
 that students receive accurately reflect their submissions.
 
-The Cormen-lib testing suite offers extremely lightweight and flexible unit testing utilities that can be used on any
+The Cormen-Lib testing suite offers extremely lightweight and flexible unit testing utilities that can be used on any
 kind of assignment, whether to write functions or build classes. Course administration can be easily streamlined by
 restricting which library data structures students are allowed to use on any particular assignment.
 
-Cormen-lib began as a project by two Brandeis University undergraduate students to replace hand-written problem sets
+Cormen-Lib began as a project by two Brandeis University undergraduate students to replace hand-written problem sets
 written in pseudocode.
 
 ## Provided Data Structures
-The Cormen-lib library offers a set of fundamental data structures and algorithms, with behavior as specified by H.
+The Cormen-Lib library offers a set of fundamental data structures and algorithms, with behavior as specified by H.
 Cormen et al.'s *Introduction to Algorithms*. The following structures (separated by module) are supported:
 
 * [arrays](https://cormen-lib-developers.github.io/Cormen-Lib/arrays.html)
@@ -43,7 +43,7 @@ Cormen et al.'s *Introduction to Algorithms*. The following structures (separate
     * Graph
 
 ## Unit Testing
-Along with the Cormen-lib data structures come test utilities for writing test cases. The testing framework allows a
+Along with the Cormen-Lib data structures come test utilities for writing test cases. The testing framework allows a
 course administrator to easily write test cases for either expected function output or general class behavior. Test
 cases can then be compiled into a testing suite. The testing suite has the capability to set a test case run-time 
 timeout and to record comma-separated test results for administrative use.
@@ -73,11 +73,11 @@ Consider the example test case below:
 
 ## Installation
 
-Cormen-lib is [available on PyPI](https://pypi.org/project/cormen-lib/), and can be installed with pip.
+Cormen-Lib is [available on PyPI](https://pypi.org/project/cormen-lib/), and can be installed with pip.
 
     pip install cormen-lib
 
-Cormen-lib has the following dependencies:
+Cormen-Lib has the following dependencies:
 
     Python >= 3.6
 
@@ -88,13 +88,13 @@ correctness, documentation and feature requests.
 
 ## Academic Usage
 
-If you are planning to use Cormen-lib for a university course and have questions, feel free to reach out by email.
+If you are planning to use Cormen-Lib for a university course and have questions, feel free to reach out by email.
 
 ## Documentation
 
-The full documentation for Cormen-lib is available [here](https://cormen-lib-developers.github.io/Cormen-Lib/).
+The full documentation for Cormen-Lib is available [here](https://cormen-lib-developers.github.io/Cormen-Lib/).
 
 ## Sample Usage
 
-To view sample assignments using Cormen-lib browse the [Cormen-lib sample problems repository](https://github.com/Cormen-Lib-Developers/Cormen-Lib-Sample-Problems) on github.
+To view sample assignments using Cormen-Lib browse the [Cormen-Lib sample problems repository](https://github.com/Cormen-Lib-Developers/Cormen-Lib-Sample-Problems) on github.
 """
